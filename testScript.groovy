9ce49a773910a2817ec0f6893232a5c697e7cc4e
anuragnautiyal <anu.manage@gmail.com>
2020-07-21 23:26:54 -0500

    five

--- testScript.groovy
+++ testScript.groovy
@@ -2,4 +2,12 @@ def build() {
     echo "build number ${BUILD_NUMBER} in PROGRESS"
     echo "build in DONE"
 }
- return this
+
+def deploy() {
+    echo "deploy in PROGRESS"
+    echo " the VALUE IS ${VALUE}"
+    echo "using server credentials ${CREDENTIALS}"
+    echo "deploy in DONE"
+}
+
+return this
